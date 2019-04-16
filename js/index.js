9c34de5f133d833f8c0938d42a17ad3c000b0e92
Seth Lachman <lachmanseth17@gmail.com>
2019-04-16 18:49:14 -0400

    Added sticky navigation and adjusted navigation and hero styles to account for this update.

--- js/index.js
+++ js/index.js
@@ -2,8 +2,20 @@
 const year = document.querySelector('.year');
 year.innerHTML = new Date().getFullYear();
 
+function showMobile(event) {
+  const hero = document.querySelector('.hero');
+  const mobileMenu = document.querySelector('.nav-bar');
+  if (window.scrollY > (hero.offsetTop + hero.offsetHeight)) {
+    mobileMenu.classList.add('in-view');
+  } else {
+    mobileMenu.classList.remove('in-view');
+  }
+}
+
+window.addEventListener('scroll', showMobile);
 
 // Set up animate on scroll library
 AOS.init({
   disable: "mobile"
 });
+
