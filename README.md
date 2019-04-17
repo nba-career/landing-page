f0dae810ba6ca0aced6abd0db0e09bbca018002a
Seth Lachman <lachmanseth17@gmail.com>
2019-04-17 15:46:03 -0400

    Updated README file and added github link to individual profiles.

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Are you tired of guessing which players are going to make a lasting impact in th
 
 This site serves as a marketing page for the main React application that will feature various players and attempt to predict the longevity of that player using existing data sets. Here's the [link](https://nbacareerlength.netlify.com/) to the site.
 
-I created a home page that describes the project to potential users and provides a way for users to login into the React application if they are interested in learning more about this project.
+The home page of this site describes the project to potential users and provides a way for users to login into the React application if they are interested in testing the features of this product. There is also an about page that lists all of the team members of this project.
 
-I designed both the home page and about page for this site and I also chose the font stack, colors and images for the site. The site is mobile responsive and also includes breakpoints for tablet sites as well. The LESS preprocessor is also used to style the site and manage variables for colors and media queries. 
+I designed both the home page and about page for this site and I also chose the font stack, colors and images for the site. The site is mobile responsive and also includes breakpoints for tablet sizes as well. The LESS preprocessor was used to style the site and manage variables for colors and media queries. I also researched and integrated the [Animate on Scroll library](https://github.com/michalsnik/aos) to provide different animations and transitions when the user scrolls down the page. 
 
-On the about page, I created a modal component using JavaScript to display the biographies of each team member.
+I used an array of objects to populate the navigation menu so that I wouldn't need to repeat the content on the home page and the about page.
+
+I created a Class constructor and used an array of objects to populate the team data (titles, names, and images). 
+
+On the about page, I created a modal component using JavaScript to display the biographies of each team member. If a new team member is added or removed from this project, the component will work without breaking.
 
--- js/team-member-info.js
+++ js/team-member-info.js
@@ -1,43 +1,50 @@
 const teamMemberInfo = [
 	{
 		name: 'Troy Bradley',
-		img: 'https://github.com/bitcointroy.png',
+    img: 'https://github.com/bitcointroy.png',
+    link: 'https://github.com/bitcointroy',
 		title: 'Team Lead',
 		bio: 'Lorem ipsum stuff'
 	},
 	{
 		name: 'Brooks Vinyard',
-		img: 'https://github.com/brooksvinyard.png',
+    img: 'https://github.com/brooksvinyard.png',
+    link: 'https://github.com/brooksvinyard',
 		title: 'Backend Developer',
 		bio: 'Lorem ipsum stuff'
 	},
 	{
 		name: "Daniel O'Neill",
-		img: 'https://github.com/danpatrickoneill.png',
+    img: 'https://github.com/danpatrickoneill.png',
+    link: 'https://github.com/danpatrickoneill',
 		title: 'Frontend Developer',
 		bio: 'Lorem ipsum stuff'
 	},
 	{
 		name: 'Seth Lachman',
-		img: 'https://github.com/stlachman.png',
+    img: 'https://github.com/stlachman.png',
+    link: 'https://github.com/stlachman',
 		title: 'UI Developer',
 		bio: 'I developed the User Interface for this marketing site!'
 	},
 	{
 		name: 'Rick Clayton',
-		img: 'https://github.com/rick1270.png',
+    img: 'https://github.com/rick1270.png',
+    link: 'https://github.com/rick1270',
 		title: 'Data Science',
 		bio: 'Lorem ipsum stuff'
 	},
 	{
 		name: 'Daniel Carter',
-		img: 'https://github.com/tesseract314.png',
+    img: 'https://github.com/tesseract314.png',
+    link: 'https://github.com/tesseract314',
 		title: 'Data Science',
 		bio: 'Lorem ipsum stuff'
 	},
 	{
 		name: 'Christopher Tolbert',
-		img: 'https://github.com/hughjafro.png',
+    img: 'https://github.com/hughjafro.png',
+    link: 'https://github.com/hughjafro',
 		title: 'Data Science',
 		bio: 'Lorem ipsum stuff'
 	}
@@ -51,6 +58,7 @@ class TeamMember {
     this.image = this.teamCard.querySelector('img');
     this.modalName = this.teamCard.querySelector('.modal--content h3');
     this.content = this.teamCard.querySelector('.modal--content p');
+    this.githubLink = this.teamCard.querySelector('.modal--content a');
   
 
 		this.teamMemberInfo = teamMemberInfo;
@@ -59,7 +67,8 @@ class TeamMember {
     this.modalName.textContent = teamMemberInfo.name;
     this.image.src = teamMemberInfo.img;
     this.image.alt = teamMemberInfo.name;
-		this.content.textContent = teamMemberInfo.bio;
+    this.content.textContent = teamMemberInfo.bio;
+    this.githubLink.href = teamMemberInfo.link;
 	}
 }
 
